2d0faad79490ddf99874aa8b1e0a1d3733c9e4e5
utzeln <nadine.utzel@iter.org>
2018-09-05 13:33:31 +0000

    Run script to start all required services for the tests

--- products/org.csstudio.iter.css.product/demo/m-TEST/src/main/run.sh
+++ products/org.csstudio.iter.css.product/demo/m-TEST/src/main/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#*******************************************************************************
+# * Copyright (c) 2010-2018 ITER Organization.
+# * All rights reserved. This program and the accompanying materials
+# * are made available under the terms of the Eclipse Public License v1.0
+# * which accompanies this distribution, and is available at
+# * http://www.eclipse.org/legal/epl-v10.html
+# ******************************************************************************
 
 function softIoc {
     ######################################################################
@@ -43,10 +50,16 @@ function alarm {
     cmds[1]="bash -c 'alarm-configtool -root demo -import -file ./beast/CTRL-beast.xml ; alarm-server -root demo'"
     titles[1]="alarm-server -root demo"
     
-    cmds[2]="bash -c 'alarm-configtool -root UTIL -import -file ./beast/UTIL-beast.xml ; alarm-server -root UTIL'"
-    titles[2]="alarm-server -root UTIL"
+    cmds[2]="bash -c 'alarm-notifier -root demo'"
+    titles[2]="alarm-notifier -root demo"
     
-    for i in {1..2}; do
+    cmds[3]="bash -c 'alarm-configtool -root UTIL -import -file ./beast/UTIL-beast.xml ; alarm-server -root UTIL'"
+    titles[32]="alarm-server -root UTIL"
+    
+    cmds[4]="bash -c 'jms2rdb -pluginCustomization ./beast/jms.ini'"
+    titles[4]="jms2rdb topic demo"
+    
+    for i in {1..4}; do
         options+=($tab -t "\"${titles[i]}\"" -e "\"${cmds[i]}\"")
     done
 }
@@ -62,7 +75,25 @@ function archive {
     options=" --profile='default'"
     
     cmds[1]="bash -c 'archive-configtool -engine demo -port 5812 -import -config ./beauty/CTRL-beauty.xml -replace_engine; archive-engine -engine demo -port 5812'"
-   titles[1]="archive-engine -engine demo -port 5812"
+    titles[1]="archive-engine -engine demo -port 5812"
+    
+    for i in {1..1}; do
+        options+=($tab -t "\"${titles[i]}\"" -e "\"${cmds[i]}\"")
+    done
+}
+
+function css {
+    ######################################################################
+    # Configure and start cs-studio
+    ######################################################################
+    
+    echo -e "\n\tConfigure and start cs-studio...\n"
+    
+    tab=" --tab"
+    options=" --profile='default'"
+    
+    cmds[1]="bash -c 'cp ./demo_beast.xml /tmp/demo_beast.xml; sudo cp /tmp/demo_beast.xml /opt/codac/css/css/configuration/diirt/datasources/beast/beast.xml; sudo boy-switch-resolution 4k; css'"
+    titles[1]="Start cs-studio"
     
     for i in {1..1}; do
         options+=($tab -t "\"${titles[i]}\"" -e "\"${cmds[i]}\"")
@@ -71,14 +102,20 @@ function archive {
 
 date
 
+rm -Rf ~/.css
+
 softIoc
 
 alarm
 
 archive
 
+css
+
 CMD="gnome-terminal "${options[@]}""
 echo -e "$CMD"
 eval "$CMD"
+
+date
 
 exit 0
